a937ac4b7b67ce12ec228724559b740d236eaf99
Drew Lawrence <drewlawrence0625@gmail.com>
2024-05-30 07:37:51 -0500

    New Update to project

--- scripts/validation.js
+++ scripts/validation.js
@@ -71,7 +71,7 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save_button",
-  inactiveButtonClass: "modal__save_button-disabled",
+  inactiveButtonClass: "modal__save_button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error",
 };
